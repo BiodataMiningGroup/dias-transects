8498116dfe5d03ec7b3dc82f7241483c5977f772
Martin Zurowietz <martin@zurowietz.de>
2020-07-08 16:53:25 +0200

    Remove files to make a stub module

---
+++ README.md
@@ -0,0 +1,5 @@
+# BIIGLE Volumes Module
+
+This is the BIIGLE module to create, edit and manage volumes.
+
+**Deprecated:** This module is deprecated as the features have been merged into [biigle/core](https://github.com/biigle/core).
--- resources/views/volumes/searchTab.blade.php
+++
@@ -1,3 +0,0 @@
-<li role="presentation" @if($type === 'volumes') class="active"@endif>
-    <a href="{{route('search', ['q' => $query, 't' => 'volumes'])}}">Volumes <span class="badge">{{readable_number($volumeResultCount)}}</span></a>
-</li>
